51819cbb116f86f99792dd16f817a1b84cfc49d9
Aleks-Vecher <aleks.vecher@gmail.com>
2023-01-23 18:32:39 +0300

    authentication finished

---
+++ src/components/sign-in-form/sign-in-form.component.jsx
@@ -0,0 +1,89 @@
+import { useState } from 'react'
+import FormInput from '../form-input/form-input.component'
+import Button from '../button/button.component'
+import {
+  signInWithGooglePopup,
+  createUserDocumentFromAuth,
+  signInAuthUserWithEmailAndPassword
+} from '../../utils/firebase/firebase.utils'
+import './sign-in-form.style.scss'
+
+const defaultFormfields = {
+  email: '',
+  password: '',
+}
+
+const SignInForm = () => {
+  const [formFields, setFormFields] = useState(defaultFormfields)
+  const { email, password } = formFields
+
+  const handleSubmit = async (event) => {
+    event.preventDefault()
+
+    try {
+      const response = await signInAuthUserWithEmailAndPassword(email, password)
+      console.log(response);
+      resetFormField()
+    } catch (error) {
+      switch (error.code) {
+        case 'auth/user-not-found':
+          alert('no user associated with this email')
+          break
+        case 'auth/wrong-password':
+          alert('incorrect password for email')
+          break
+        default:
+          console.log(error);
+      }
+    }
+
+  }
+
+  const handleChange = (event) => {
+    const { name, value } = event.target
+    setFormFields({ ...formFields, [name]: value })
+  }
+
+  const resetFormField = () => {
+    setFormFields(defaultFormfields)
+  }
+
+  const signInWithGoogle = async () => {
+    const { user } = await signInWithGooglePopup()
+    await createUserDocumentFromAuth(user)
+  }
+
+  return (
+    <div className='sign-up-container' >
+      <h2>Already have an accaount?</h2>
+      <span>
+        Sing in with your email and password
+      </span>
+      <form onSubmit={handleSubmit} >
+        <FormInput
+          label='Email'
+          type='email'
+          required
+          onChange={handleChange}
+          name='email'
+          value={email}
+        />
+
+        <FormInput
+          label='Password'
+          type='password'
+          required
+          onChange={handleChange}
+          name='password'
+          value={password}
+        />
+        <div className='buttons-container' >
+          <Button type='submit'>Sign in</Button>
+          <Button type='button' buttonType='google' onClick={signInWithGoogle} >Google sign in</Button>
+        </div>
+      </form>
+    </div>
+  )
+}
+
+export default SignInForm
--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -1,5 +1,5 @@
 import { initializeApp } from 'firebase/app'
-import { getAuth, signInWithRedirect, signInWithPopup, GoogleAuthProvider, createUserWithEmailAndPassword } from 'firebase/auth'
+import { getAuth, signInWithPopup, GoogleAuthProvider, createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth'
 import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore'
 
 const firebaseConfig = {
@@ -21,12 +21,11 @@ googleProvider.setCustomParameters({
 
 export const auth = getAuth()
 export const signInWithGooglePopup = () => signInWithPopup(auth, googleProvider)
-export const signInWithGoogleRedirect = () => signInWithRedirect(auth, googleProvider)
 
 export const db = getFirestore()
 
 export const createUserDocumentFromAuth = async (userAuth, additionalInformation = {}) => {
-  if(!userAuth) return
+  if (!userAuth) return
 
   const userDocRef = doc(db, 'users', userAuth.uid)
   const userSnapshot = await getDoc(userDocRef)
@@ -51,7 +50,12 @@ export const createUserDocumentFromAuth = async (userAuth, additionalInformation
 }
 
 export const createAuthUserWithEmailAndPassword = async (email, password) => {
-  if(!email || !password) return
+  if (!email || !password) return
 
   return await createUserWithEmailAndPassword(auth, email, password)
+}
+export const signInAuthUserWithEmailAndPassword = async (email, password) => {
+  if (!email || !password) return
+
+  return await signInWithEmailAndPassword(auth, email, password)
 }

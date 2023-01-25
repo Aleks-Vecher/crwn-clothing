3cb56cc256a4ac2e1a27069dae43225e5b1c5a11
Aleks-Vecher <aleks.vecher@gmail.com>
2023-01-25 15:34:53 +0300

    added in our addToCart feature

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -4,14 +4,12 @@ import { CartContext } from '../../context/cart.context'
 import './cart-icon.style.scss'
 
 const CartIcon = () => {
-  const { isCartOpen, setIsCartOpen } = useContext(CartContext)
+  const { setIsCartOpen, cartCount } = useContext(CartContext)
 
-
-  const toggleIsCartOpen = () => setIsCartOpen(!isCartOpen)
   return (
-    <div className='cart-icon-container' onClick={toggleIsCartOpen} >
+    <div className='cart-icon-container' onClick={setIsCartOpen} >
       <ShoppingIcon className='shopping-icon' />
-      <span className='item-count'>0</span>
+      <span className='item-count'>{cartCount}</span>
     </div>
   )
 }
